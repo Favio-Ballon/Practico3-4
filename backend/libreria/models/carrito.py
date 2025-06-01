273467aad2343635e4e7345e1b2542fc11817d07
favio-ballon <favioballon@gmail.com>
2025-06-01 05:15:13 -0400

    models

--- backend/libreria/models/carrito.py
+++ backend/libreria/models/carrito.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 
-class carrito(models.Model):
-    libro = models.ForeignKey('libreria.Libro', on_delete=models.CASCADE)
+class Carrito(models.Model):
+    libros = models.ManyToManyField('libreria.Libro', related_name='carritos')
     precio_total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    libro = models.ManyToManyField('libreria.Libro', related_name='carritos_libro')
+    usuario = models.ForeignKey(
+        'auth.User', on_delete=models.CASCADE, related_name='carritos', null=True, blank=True
+    )
 
     def __str__(self):
-        return f"Carrito {self.id} - Libro: {self.libro.titulo} - Cantidad: {self.cantidad}"
+        return f"Carrito {self.id} - Libro: {self.libro.titulo} "
