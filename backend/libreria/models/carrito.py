ad45d15ee56bb55a6ab646eb6d0eb43e1df47254
favio-ballon <favioballon@gmail.com>
2025-06-01 06:11:26 -0400

    rm extra libros

--- backend/libreria/models/carrito.py
+++ backend/libreria/models/carrito.py
@@ -4,7 +4,6 @@ from django.db import models
 class Carrito(models.Model):
     libros = models.ManyToManyField('libreria.Libro', related_name='carritos')
     precio_total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    libro = models.ManyToManyField('libreria.Libro', related_name='carritos_libro')
     usuario = models.ForeignKey(
         'auth.User', on_delete=models.CASCADE, related_name='carritos', null=True, blank=True
     )
